343de86f47e935db6bb956e714b173c9a87a0a79
Daniel Kaiser <danielkaiser@fastmail.com>
2020-02-03 23:19:50 +0100

    Use newer Date classes

--- JettyExample/src/main/java/meinbeanpackage/MeineJavaBean.java
+++ JettyExample/src/main/java/meinbeanpackage/MeineJavaBean.java
@@ -1,11 +1,11 @@
 package meinbeanpackage;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-@SuppressWarnings("WeakerAccess")
 public class MeineJavaBean {
     public String getDateString() {
-        return (new SimpleDateFormat("yyyy-MM-dd, HH:mm:ss")).format(new Date()) + " h";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
     }
 }
