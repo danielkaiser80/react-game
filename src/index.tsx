99a9d475b97e7be67a587e797cec144c02593728
Daniel Kaiser <danielkaiser@fastmail.com>
2023-03-31 14:15:56 +0200

    Use vite instead of CRA for handling of build and deployment; test currently excluded.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./Game";
+import { render } from "react-dom";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+render(<Game />, document.getElementById("root"));
