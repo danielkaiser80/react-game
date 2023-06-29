c96b15372efb001e9f7208266d3c407564b9f2c2
Daniel Kaiser <danielkaiser@fastmail.com>
2023-06-29 22:18:39 +0200

    Update to the latest available versions.

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const path = require("path");
-const camelcase = require("camelcase");
+const camelcase = (...args) =>
+  import(camelcase).then(({ default: camelcase }) => camelcase(...args));
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
-
 module.exports = {
   process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
